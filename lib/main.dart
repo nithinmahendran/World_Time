c033a526b5d530acc836abeeb7bb95b5120e1987
Nithin Mahendran <nithinmahendren@gmail.com>
2021-03-11 19:12:31 +0530

    Included World Time Api

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,15 @@ import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 
-
-
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/home',
-    routes: {
-      '/':(context) =>Loading(),
-      '/home':(context)=>Home(),
-      '/location':(context)=>ChooseLocation(),
-    },
+  runApp(
+    MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
     ),
-    );
+  );
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String location; //location name
+  String time; //Time of that particular location
+  String flag; // url for the assets
+  String url; //Location Url for the api
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getData() async {
+    //making the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+    Map data = jsonDecode(response.body);
+    //print(data);
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    String offsetmin = data['utc_offset'].substring(4, 6);
+    //print(datetime);
+    //print(offset);
+
+    //create a datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now
+        .add(Duration(hours: int.parse(offset), minutes: int.parse(offsetmin)));
+    //print(now);
+
+    //converting to string
+    time = now.toString();
+  }
+}
